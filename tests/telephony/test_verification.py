c80ca3f9c2f5e21aaa8e3b53174519ce3dcee91e
Nina Zakharenko <nzakharenko@gmail.com>
2020-04-13 19:51:44 -0700

    Add tests around handling affirmative and negative verification messages

--- tests/telephony/test_verification.py
+++ tests/telephony/test_verification.py
@@ -3,6 +3,7 @@ from unittest import mock
 import nexmo
 import pytest
 from hotline import injector
+from hotline.database import highlevel as db
 from hotline.telephony import verification
 from tests.telephony import helpers
 
@@ -57,3 +58,56 @@ def test_start_member_verification_no_primary_event_number(sleep, database):
     client.send_message.assert_called_once_with(
         {"from": hotline_virtual_number, "to": member.number, "text": expected_msg}
     )
+
+
+@mock.patch("time.sleep", autospec=True)
+def test_handle_verification_affirmative_message(sleep, database):
+    client = mock.create_autospec(nexmo.Client, instance=True)
+
+    client.application_id = "appid"
+    client.send_message.return_value = {"messages": [{"error-text": ""}]}
+
+    injector.set("nexmo.client", client)
+
+    hotline_virtual_number = "1234567890"
+    injector.set("secrets.virtual_number", hotline_virtual_number)
+
+    event = helpers.create_event()
+
+    responses = ("ok", "yes", "okay")
+    for response in responses:
+        member = helpers.add_unverfied_members(event)
+        verification.maybe_handle_verification(member.number, response)
+
+        member = db.get_member_by_number(member.number)
+        assert member.verified
+
+        expected_msg = "Thank you, your number is confirmed."
+        client.send_message.assert_called_with(
+            {"from": event.primary_number, "to": member.number, "text": expected_msg}
+        )
+
+
+@mock.patch("time.sleep", autospec=True)
+def test_handle_verification_negative_message(sleep, database):
+    client = mock.create_autospec(nexmo.Client, instance=True)
+
+    client.application_id = "appid"
+    client.send_message.return_value = {"messages": [{"error-text": ""}]}
+
+    injector.set("nexmo.client", client)
+
+    hotline_virtual_number = "1234567890"
+    injector.set("secrets.virtual_number", hotline_virtual_number)
+
+    event = helpers.create_event()
+
+    responses = ("nyet", "nay", "no way")
+    for response in responses:
+        member = helpers.add_unverfied_members(event)
+        verification.maybe_handle_verification(member.number, response)
+
+        member = db.get_member_by_number(member.number)
+        assert not member.verified
+
+        client.send_message.assert_not_called()
